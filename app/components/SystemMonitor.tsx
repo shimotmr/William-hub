0145dc9cb84ce24597a0a1d6ff37e0db68df1b5f
Travis <travis@TravisdeMac-mini.local>
2026-02-18 00:19:24 +0800

    fix: mobile 2-col grid for system status cards

--- app/components/SystemMonitor.tsx
+++ app/components/SystemMonitor.tsx
@@ -161,7 +161,7 @@ export default function SystemMonitor() {
       </div>
 
       {/* Cards Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-3 gap-3">
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-3 gap-3">
         {cards.map((card, i) => (
           <div
             key={i}
